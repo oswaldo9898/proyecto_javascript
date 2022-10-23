d1b54215d9b9511c55412aed57a540065a0b78a3
oswaldogarces98 <oswaldogarces98@gmail.com>
2022-10-23 18:48:47 -0500

    tercera entrega

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,147 @@
-let cantProductos = 0,
-    nombreProducto = '',
+let codigo = 0,
+    cantProductos = 0,
+    detalleProducto = '',
     cantidadProducto = 0,
-    precioProducto = 0.0,
-    subTotal = 0.0,
-    total = 0.0;
+    precioUnitarioProducto = 0.0,
+    precioTotal = 0.0;
     
-const iva = 0.12;
-const arrProductos = [];
+const porcentajeIVA = 0.12;
+let arrProductos = [];
 
+let productTableRef = document.querySelector('#productTable');
+let subtotalPagar = document.querySelector('#subtotalPagar');
+let ivaPagar = document.querySelector('#ivaPagar');
+let totalPagar = document.querySelector('#totalPagar');
+const frmProducto = document.getElementById('formProducto');
 
 // CLASE PRODUCTOS
 class Producto {
-    constructor(nombre, cantidad, precioUnitario){
-        this._nombre = nombre;
-        this._cantidad = cantidad;
-        this._precioUnitario = precioUnitario;
-    }
-
-    // METODOS PARA OBTENER Y MODIFICAR VALORES
-    getNombre() {
-        return this._nombre;
-    }
-
-    setNombre(nombre) {
-        this._nombre = nombre;
-    }
-
-    getCantidad() {
-        return this._cantidad;
-    }
-
-    setCantidad(cantidad) {
-        this._cantidad = cantidad;
-    }
-
-    getPrecioUnitario() {
-        return this._precioUnitario;
-    }
-
-    setPrecioUnitario(precioUnitario) {
-        this._precioUnitario = precioUnitario;
-    }
-
-    // METODOS GENERALES DE LA CLASE
-    totalProducto() {
-        return this._cantidad * this._precioUnitario;
+    constructor(codigo, detalle, cantidad, precioUnitario,precioTotal){
+        this.codigo = codigo;
+        this.detalle = detalle;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.precioTotal = precioTotal
     }
 }
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    arrProductos = JSON.parse(localStorage.getItem("productos")) || [];
+    agregarProductosTabla();
+});
 
-alert('Programa para calcular el monto total a pagar, en caso del que el monto sea mayor a $60.00 dolares se aplica un descuento del 5%');
 
-do{
-    cantProductos = prompt('Ingrese la cantidad de productos a registrar: ');
-}while(isNaN(parseInt(cantProductos)));
+/** ESTA FUNCION SE ENCARGA DEL CALCULO DE LOS VALORES
+- SUBTOTAL
+- IVA
+- TOTAL */
+const calcularMontosPagar = () => {
+    let subTotal = 0.0;
+    let total = 0.0;
 
-for(let i=1; i <= cantProductos; i++){
-    do{
-        nombreProducto = prompt(`Ingrese el nombre del producto ${i}:`);
-        cantidadProducto = parseInt(prompt(`Ingrese la cantidad del producto ${i}:`));
-        precioProducto = parseFloat(prompt(`Ingrese el precio del producto ${i}:`));
+    if(arrProductos.length > 0 ){
+        arrProductos.forEach((element) => {
+            subTotal += (element.precioUnitario * element.cantidad);
+        });
+    }
 
-        let nuevoProducto = new Producto(nombreProducto, cantidadProducto, precioProducto);
-        arrProductos.push( nuevoProducto);
+    let iva = subTotal * porcentajeIVA;
+    total = subTotal + iva;
 
-    }while(Number.isNaN(precioProducto));
-}
+    subtotalPagar.textContent = `$${subTotal.toFixed(2)}`;
+    ivaPagar.textContent = `$${iva.toFixed(2)}`;
+    totalPagar.textContent = `$${total.toFixed(2)}`;
+};
 
-// METODO PARA CALCULAR EL TOTAL A PAGAR POR LOS PRODUCTOS
-const calcularTotal = () => {
-    arrProductos.forEach((element) => {
-        console.log(element);
-        subTotal += element.totalProducto();
+
+/** ESTA FUNCION PERMITE AGREGAR LOS REGISTROS QUE ESTEN CARGADOS EN
+EL ARRAY arrProductos */
+const agregarProductosTabla = () => {
+    productTableRef.innerHTML = ``;
+
+    arrProductos.forEach((element, index) => {
+        productTableRef.innerHTML += `<tr>
+            <td>${index+1}</td>
+            <td>${element.detalle}</td>
+            <td class="text-center">${element.cantidad}</td>
+            <td class="text-center">${element.precioUnitario.toFixed(2)}</td>
+            <td class="text-center">${element.precioTotal.toFixed(2)}</td>
+            <td class="text-center"><a href="#" id="${element.codigo}" class="btn btn-outline-danger borrar-curso" data-id="1"><i
+            class="fa-solid fa-trash" ></i> Eliminar</a></td>
+        </tr>`;
+
+    });
+    calcularMontosPagar();
+
+    let botonEliminar = document.querySelectorAll('#productTable tr td a');
+    botonEliminar.forEach(element => {
+        element.addEventListener('click',(e)=>{
+
+            Swal.fire({
+                title: '¿Está seguro?',
+                text: "No podrás revertir esto.!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminar!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    e.preventDefault();
+                    let id = parseInt(e.target.id);
+                    arrProductos = arrProductos.filter( producto => producto.codigo !== id );
+                    guardarLocalStorage();
+                    agregarProductosTabla()
+
+                    Swal.fire(
+                        'Eliminado!',
+                        'El registro ha sido eliminado.',
+                        'Éxito'
+                    )
+                }
+            });
+        })
     })
 
-    total = subTotal + (subTotal*iva);
-
-    if(total > 60.00){
-        let totalDescuento = total-(total*0.05)
-        alert(`El subtotal es: ${subTotal.toFixed(2)} - Felicidades su total es mayor a $60, y es acreedor a un descuento del 5%, el total a pagar es: $${totalDescuento.toFixed(2)}`);
-    }else{
-        alert(`El subtotal es: ${subTotal.toFixed(2)} - El total a pagar con iva es: ${total.toFixed(2)}`)
-    }
-}
+};
 
 
-calcularTotal();
+/** FUNCION QUE SE ALMACENA EL ARRAY DE PRODUCTOS EN EL LOCALSTORAGE CADA 
+QUE SE AGREGA UN NUEVO PRODUCTO */
+const guardarLocalStorage = () => {
+    localStorage.setItem("productos", JSON.stringify(arrProductos));
+};
 
+
+/** EVENTO SUBMIT DEL FORMULARIO */
+frmProducto.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let datosFormulario = new FormData(frmProducto);
+    agregarProducto(datosFormulario);
+    frmProducto.reset();
+});
+
+
+/** FUNCION MEDIANTE LA CUAL SE AGREGA UN NUEVO PRODUCTO AL ARRAY 
+ * arrProducto
+ */
+const agregarProducto = (datosFormulario) => {
+    codigo = Math.floor(Math.random() * 10000);
+    detalleProducto = datosFormulario.get('detalle');
+    cantidadProducto = parseInt(datosFormulario.get('cantidad'));
+    precioUnitarioProducto = parseFloat(datosFormulario.get('precioUnitario'));
+    precioTotal = (cantidadProducto * precioUnitarioProducto);
+
+    let nuevoProducto = new Producto(codigo, detalleProducto, cantidadProducto, precioUnitarioProducto, precioTotal);
+    arrProductos.push(nuevoProducto);
+
+    guardarLocalStorage();
+    agregarProductosTabla();
+
+    
+};
 
 
 
